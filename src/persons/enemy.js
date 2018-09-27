abebeaf6929bda9832d4ed8b6f3a00822b188efc
TRAD3R <6292676@gmail.com>
2018-09-27 11:06:30 +0300

    create persons

---
+++ src/persons/enemy.js
@@ -0,0 +1,3 @@
+export default (x = 110, y = 320, pacX = 0, pacY = 0, dirX = 0, dirY = 0, size = 32, speed = 5) => {
+  return {x, y, pacX, pacY, dirX, dirY, size, speed};
+}
---
+++ src/persons/powerdot.js
@@ -0,0 +1,8 @@
+export default {
+  x: 110,
+  y: 310,
+  powerUp: false,
+  pCountDown: 0,
+  ghostNum: 0,
+  ghostEat: false
+}
